a1fb6c226dbe312a95af0d6b37ac802cd1ac717c
samithuang <285365963@qq.com>
2026-09-07 07:07:38 +0000

    Revert local vLLM 0.27 compatibility shims to match upstream/main
    Signed-off-by: samithuang <285365963@qq.com>

--- vllm_omni/config/endpoint_policy.py
+++ vllm_omni/config/endpoint_policy.py
@@ -10,14 +10,7 @@ from typing import NamedTuple
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from starlette.routing import Route
-
-try:
-    from vllm.entrypoints.serve import create_error_response
-except ImportError:
-    try:
-        from vllm.entrypoints.serve.exception_handling.error_response import create_error_response
-    except ImportError:
-        from vllm.entrypoints.serve.utils.error_response import create_error_response
+from vllm.entrypoints.serve.exception_handling.error_response import create_error_response
 
 
 class RouteTarget(NamedTuple):
