003750790b6e35c5387f0518fa238545f0c42fa0
samithuang <285365963@qq.com>
2026-09-07 07:11:20 +0000

    Fix ReadTheDocs CI failure caused by MkDocs 2.0 migration warning banner
    - Suppress MkDocs 2.0 warning banners in docs/mkdocs/hooks/search_index.py via NO_MKDOCS_2_WARNING and DISABLE_MKDOCS_2_WARNING
    - Pin mkdocs-material<9.6.0 in pyproject.toml docs extra
    - Set fail_on_warning: false in .readthedocs.yml so external banner warnings do not fail strict builds
    
    Signed-off-by: samithuang <285365963@qq.com>

--- docs/mkdocs/hooks/search_index.py
+++ docs/mkdocs/hooks/search_index.py
@@ -1,6 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
-# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM-Omni project
 """Reduce browser search-index memory for generated API reference pages."""
+
+import os
+
+# Suppress MkDocs 2.0 migration warnings from mkdocs-material and plugins
+os.environ["NO_MKDOCS_2_WARNING"] = "true"
+os.environ["DISABLE_MKDOCS_2_WARNING"] = "true"
 
 from material.plugins.search.plugin import SearchIndex
 
